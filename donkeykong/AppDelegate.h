6286730425ea2583d7e27ffb2031dd47c59d75e6
jgavin <jgavin@nd.edu>
2014-03-18 03:02:22 -0400

    Initial Commit

---
+++ donkeykong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  donkeykong
+//
+//  Created by Jacob Gavin on 3/18/14.
+//  Copyright (c) 2014 Jacob Gavin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ donkeykong/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  donkeykong
+//
+//  Created by Jacob Gavin on 3/18/14.
+//  Copyright (c) 2014 Jacob Gavin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
