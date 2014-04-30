7ea6b75bf25ada7be6e647bab61064cb621eac7d
Ryan Austin Moran <rmoran@student01.cse.nd.edu>
2014-04-30 01:59:50 -0400

    added a few hammer.h comments

--- donkeykong/Hammer.cpp
+++ donkeykong/Hammer.cpp
@@ -1,9 +1,8 @@
 //  Created by Ryan Moran on 4/1/14.
 //  Copyright (c) 2014 Ryan Moran. All rights reserved.
 
-////NOTE THAT FOR NOW USES PINK SPRITESHEET: this ended up being much simpler than trying to use two sheets
 
-#include "Object.h"
+#include "Object.h" //using inheritance to access base class functions
 #include "Hammer.h"
 #include <math.h>
 #include <iostream>
@@ -17,7 +16,7 @@ Hammer::Hammer ():Object (18, 10, 45, 315, 0, 0, 0, 1, 1, 1, 0, 0, 100)
   setAnimation ();
 }
 
-
+//note that once mario grabs the hammer, we actually switch to combined images of mario and the hammer, which can be seen as case(s) in mario.cpp setAnimation
 
 //Function sets animation parameters according to state of mario
 void
@@ -26,8 +25,8 @@ Hammer::setAnimation ()
   switch (currentState)
     {
     case 1:
-      spritesheetx = 195;
-      spritesheety = 263;
+      spritesheetx = 195; //x location on sprite sheet
+      spritesheety = 263; //y location on sprite sheet
       currentFrame = 0;
       maxFrames = 1;
       height = 18;
